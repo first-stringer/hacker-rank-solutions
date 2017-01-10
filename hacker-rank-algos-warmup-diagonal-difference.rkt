ccc8345a8fe0431fa3827cf72a6a945898eb91e3
first-stinger <justin.michael.stringer@gmail.com>
2017-01-10 09:56:06 -0600

    added comment

--- hacker-rank-algos-warmup-diagonal-difference.rkt
+++ hacker-rank-algos-warmup-diagonal-difference.rkt
@@ -2,6 +2,16 @@
 
 ;; https://www.hackerrank.com/challenges/a-very-big-sum
 
+#| 
+This solution looks overly complex.  I designed it to process the matrix of positions as I read each position.  
+I did this to avoid creating a large 2D array in memory and to avoid processing every position twice. 
+First I create the list of positions I am interested in and classify each one as l for left or r for right.
+Then I read in the matrix positions and check each one against my list.  If the position is in the list of positions 
+I am interested in, I add it to a new list if it's left and I add it's negative to the new list if it's right.  I 
+ignore the center position in odd size matrices.  I also have to sort the list of positions I am interested in after I
+create it so that it matches the order I read the matrix positions in.
+|#
+
 (define-struct e (val side) #:transparent)
 
 (define (generate-positions-list-helper n r)
