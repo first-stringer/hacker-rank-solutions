129c8e2e32b88b7bb69c711eea902aa07402802e
first-stinger <justin.michael.stringer@gmail.com>
2017-01-10 09:29:58 -0600

    added link and comment

--- hacker-rank-algos-warmup-a-very-big-sum.rkt
+++ hacker-rank-algos-warmup-a-very-big-sum.rkt
@@ -1,5 +1,9 @@
 #lang racket
 
+;; https://www.hackerrank.com/challenges/a-very-big-sum
+
+;; Sums the numbers recursively while reading them.
+
 (define (add-number n sum)
   (if (> n 0) 
     (add-number (- n 1) (+ sum (read)))
@@ -8,3 +12,4 @@
   )
 
 (add-number (read) 0)
+
