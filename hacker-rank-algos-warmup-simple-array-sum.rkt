e8f6af29086280ee512626a7cd8d4c90216dbb78
first-stinger <justin.michael.stinger@gmail.com>
2017-01-09 19:19:52 -0600

    easy peasy

---
+++ hacker-rank-algos-warmup-simple-array-sum.rkt
@@ -0,0 +1,11 @@
+#lang racket
+
+
+(define (simple-array-sum n sum)
+  (if (> n 0)
+    (simple-array-sum (- n 1) (+ sum (read)))
+    sum
+    )
+  )
+
+(simple-array-sum (string->number (read-line)) 0)
