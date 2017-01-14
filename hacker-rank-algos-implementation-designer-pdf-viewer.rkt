22948276336ca43aed4c9b17fbb441289193c58d
first-stinger <justin.michael.stringer@gmail.com>
2017-01-14 06:08:35 -0600

    add 11th solutionk

---
+++ hacker-rank-algos-implementation-designer-pdf-viewer.rkt
@@ -0,0 +1,39 @@
+#lang racket
+
+(define (read-heights n)
+  (if (> n 0)
+    (cons (read) (read-heights (- n 1)))
+    (begin 
+      (read-byte)
+      '() 
+      )
+    )
+  )
+
+(define heights (read-heights 26))
+
+(define (read-word)
+  (let ([w (read-char)])
+    (if (char-whitespace? w)
+      '()
+      (cons w (read-word))
+      )
+    )
+  )
+
+(define word (string->list (read-line)))
+
+(define (find-tallest word t) 
+  (if (empty? word) 
+    t
+    (let ([nt (list-ref heights (- (char->integer (car word)) (char->integer #\a)))])
+      (if (> nt t) 
+	(find-tallest (cdr word) nt)
+	(find-tallest (cdr word) t)
+	)
+      )
+    )
+  )
+
+(displayln (* (find-tallest word 0) (length word)))
+
