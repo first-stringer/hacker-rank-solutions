705a06315f533f2a5d358b8c87ef965d08389895
first-stinger <justin.michael.stringer@gmail.com>
2017-01-15 06:12:14 -0600

    adding 13th solution

---
+++ hacker-rank-algos-implementation-kangaroo.rkt
@@ -0,0 +1,18 @@
+#lang racket
+
+(define (check-positions x1 v1 x2 v2)
+  (cond
+    [(= x1 x2) "YES"]
+    [(and (> x1 x2) (>= v1 v2)) "NO"]
+    [(and (> x2 x1) (>= v2 v1)) "NO"]
+    [else (check-positions (+ x1 v1) v1 (+ x2 v2) v2)]
+    )
+  )
+
+(define x1 (read))
+(define v1 (read))
+(define x2 (read))
+(define v2 (read))
+
+(displayln (check-positions (+ x1 v1) v1 (+ x2 v2) v2))
+
