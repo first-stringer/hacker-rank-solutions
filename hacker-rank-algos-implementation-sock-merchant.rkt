3b2f55a5c1a4f151779f07172ec2309b7d0cca9f
first-stinger <justin.michael.stringer@gmail.com>
2017-01-17 05:40:26 -0600

    adding 17th solution

---
+++ hacker-rank-algos-implementation-sock-merchant.rkt
@@ -0,0 +1,16 @@
+#lang racket
+
+(define (process-input n st)
+  (if (= n 0)
+    0 
+    (let ([x (read)])
+      (if (set-member? st x)
+	(add1 (process-input (sub1 n) (set-remove st x)))
+	(process-input (sub1 n) (set-add st x))
+	)
+      )
+    )
+  )
+
+(displayln (process-input (read) (set)))
+
