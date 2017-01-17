3104340964e89e2493b5bd2864316fe6da78a757
first-stinger <justin.michael.stringer@gmail.com>
2017-01-17 05:48:32 -0600

    adding 18th solution

---
+++ hacker-rank-algos-implementation-utopian-tree.rkt
@@ -0,0 +1,26 @@
+#lang racket
+
+
+
+(define (get-height n)
+  (if (= n 0)
+    1
+    (if (odd? n)
+      (* 2 (get-height (sub1 n)))
+      (add1 (get-height (sub1 n)))
+      )
+    ) 
+  )
+
+;; begin provided code
+(define (get-list T)
+  (if (= T 0)
+    (list)
+    (cons (read) (get-list (- T 1)))))
+
+(let ([lst (get-list (read))])
+  (let ([ans (map get-height lst)])
+    (for ([val ans])
+      (printf "~a~%" val))))
+;; end provided code
+
