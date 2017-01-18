6c4b68ce3905a7e0e55dfbcaea24242e2ae07531
first-stinger <justin.michael.stringer@gmail.com>
2017-01-18 05:48:38 -0600

    adding 19th solution

---
+++ hacker-rank-algos-implementation-angry-professor.rkt
@@ -0,0 +1,26 @@
+#lang racket
+
+(define (read-list n)
+  (if (= n 0)
+    '()
+    (cons (read) (read-list (sub1 n)))
+    )
+  )
+
+(define (process-input ntc)
+  (when (> ntc 0)
+    (let* ([n (read)]
+	   [k (read)]
+	   [tlst (read-list n)]
+	   )
+      (if (< (- (length tlst) (count positive? tlst)) k)
+	(displayln "YES")
+	(displayln "NO")
+	)
+      )
+    (process-input (sub1 ntc))
+    )
+  )
+
+(process-input (read))
+
