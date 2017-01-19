b3b87a4cc9232325e4290324b023f24c0e463195
first-stinger <justin.michael.stringer@gmail.com>
2017-01-19 05:37:09 -0600

    adding 22nd solution

---
+++ hacker-rank-algos-implementation-save-the-prisoner.rkt
@@ -0,0 +1,21 @@
+#lang racket
+
+(define (process-input nt)
+  (when (> nt 0)
+    (let* ([n (read)]
+	   [m (read)]
+	   [s (read)]
+	   [z (+ m (sub1 s))]
+	   )
+      (cond 
+	[(<= z n) (displayln z)]
+	[(= 0 (remainder z n)) (displayln n)]
+	[else (displayln (remainder z n))]
+	)
+      )
+    (process-input (sub1 nt))
+    )
+  )
+
+(process-input (read))
+
