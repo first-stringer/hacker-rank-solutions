60347eb4a91959fc7e4f9b140a0e806df206a732
first-stinger <justin.michael.stringer@gmail.com>
2017-01-20 03:34:00 -0600

    adding 24th solution

---
+++ hacker-rank-algos-implementation-find-digits.rkt
@@ -0,0 +1,24 @@
+#lang racket
+
+(define (process-number n lst)
+  (if (empty? lst)
+    0
+    (let ([x (- (char->integer (car lst)) (char->integer #\0))])  
+      (cond
+	[(= x 0) (process-number n (cdr lst))]
+	[(= (remainder n x) 0) (add1 (process-number n (cdr lst)))]
+	[else (process-number n (cdr lst))]
+	)
+      )
+    )
+  )
+
+(define (process-input t)
+  (when (> t 0)
+    (displayln (let ([l (read-line)]) (process-number (string->number l) (string->list l))))
+    (process-input (sub1 t))
+    )
+  )
+
+(process-input (string->number (read-line)))
+
