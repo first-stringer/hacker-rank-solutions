343425a3f76faea26a075ba5ef6bf0f9e19af61b
first-stinger <justin.michael.stringer@gmail.com>
2017-01-30 05:52:35 -0600

    adding hacker rank fp solution #5

---
+++ functional-programming/hacker-rank-fp-intro-filter-array.rkt
@@ -0,0 +1,18 @@
+#lang racket
+
+(define (filter-array limit)
+  (let ([line (read-line)]) 
+    (if (non-empty-string? line) 
+      (let ([n (string->number line)])
+	(if (< n limit)
+	  (cons n (filter-array limit))
+	  (filter-array limit)
+	  )
+	)
+      '()
+      )
+    )
+  )
+
+(for-each println (filter-array (string->number (read-line))))
+
